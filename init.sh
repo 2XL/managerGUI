48f6e27f21986c37fb098092b15a8fedc64f9eaa
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-09-29 12:50:30 +0200

    Add run permisions to init.py

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 
-# init node server
-echo "start manager GUI Server"
-npm start &
-
 
 # init manager-rpc server
 echo "start manager RPC Server"
 python manager.py &
+
+
+
+# init node server
+echo "start manager GUI Server"
+npm start
