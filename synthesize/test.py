7634b23260acc7329dafffcd558f4b911ac78ed5
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-10-06 19:18:46 +0200

    Fix port listening from 2003 to 22003

--- synthesize/test.py
+++ synthesize/test.py
@@ -1,15 +1,18 @@
 import time
 import socket
 import random
-
+import wget
+import urllib2
+import json
 
 class GraphiteClient():
-    def __init__(self, host='localhost'):
+    def __init__(self, host='localhost', port='22003'):
         self.hostname = host
+        self.port = port
 
     def collect_string(self, name, val, timestamp):
         sock = socket.socket()
-        sock.connect((self.hostname, 2003))
+        sock.connect((self.hostname, self.port))
         sock.send("%s %d %d\n" % (name, val, timestamp))
         sock.close()
 
@@ -30,7 +33,9 @@ def randValue():
 
 if __name__ == "__main__":
 
-    gc = GraphiteClient('10.30.103.95')
+    graphiteUrl = 'ast03'
+    graphitePort = 22003
+    gc = GraphiteClient(graphiteUrl, graphitePort)
 
     hostname = 'Joanna'
     test_id = tsNow()
@@ -40,6 +45,30 @@ if __name__ == "__main__":
     ops = 10
     itv = 1
 
+
+    metric = '{}.{}.{}'.format(hostname, test_id, key)
+
     for x in range(0, ops, 1):
-        gc.collect_string('{}.{}.{}'.format(hostname, test_id, key), randValue(), tsNow())
+        gc.collect_string(metric, randValue(), tsNow())
         time.sleep(itv)
+
+    url = 'https://{}:8443/render/?target={}&format=json&from=-60second'.format(graphiteUrl, metric)
+    req = urllib2.Request(url)
+    res = urllib2.urlopen(req)
+    data = json.load(res)
+    print data
+
+
+
+    '''
+    CARBON_SERVER = 'ast03'
+    CARBON_PORT = 22003
+
+    message = 'foo.bar.baz 42 %d\n' % int(time.time())
+
+    print 'sending message:\n%s' % message
+    sock = socket.socket()
+    sock.connect((CARBON_SERVER, CARBON_PORT))
+    sock.sendall(message)
+    sock.close()
+    '''
