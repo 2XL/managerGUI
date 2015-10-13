bdd00afaf28d04540531a1ae018ba95b74a813f4
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-10-13 14:39:24 +0200

    Sample with 3 plots, cpu_usage, memory_usage, hdd_usage

--- static/example/index.js
+++ static/example/index.js
@@ -1,12 +1,15 @@
 (function () {
     var jsonDesc;
     // var demoURL = 'https://ast03:8443/render?target=benchbox.ast04.StackSync.cpu_usage&format=json'
-    var demoURL1 = 'https://ast03:8443/render?target=collectd.graphite.entropy.entropy&format=json'
-    var demoURL2 = 'https://ast03:8443/render?target=collectd.graphite.entropy.entropy&format=json'
-    var demoURL = 'https://ast03:8443/render?target=collectd.graphite.processes.fork_rate&format=json'
+    var demoURLCpuUsage = 'https://ast03:8443/render?from=-20minutes&until=now&target=benchbox.*.StackSync.cpu_usage&_uniq=0.24690097919665277&title=benchbox.d119.eth0.bytes_recv&format=json'
+    var demoURLRamUsage = 'https://ast03:8443/render?from=-20minutes&until=now&target=benchbox.*.StackSync.memory_usage&_uniq=0.24690097919665277&title=benchbox.d119.eth0.bytes_recv&format=json'
+    var demoURLHddUsage = 'https://ast03:8443/render?from=-20minutes&until=now&target=benchbox.*.stacksync_folder.disk_usage&_uniq=0.24690097919665277&title=benchbox.d119.eth0.bytes_recv&format=json'
+    var demoURL = 'https://ast03:8443/render?target=collectd.graphite.processes.fork_rate&format=json';
     jsonDesc = {
+
+        /*
         "Total Notifications": {
-            source: demoURL,
+            source: demoURLCpuUsage,
             GaugeLabel: {
                 parent: "#hero-one",
                 observer: function (data) {
@@ -17,7 +20,7 @@
             }
         },
         "Poll Time": {
-            source: demoURL1,
+            source: demoURLCpuUsage,
             GaugeGadget: {
                 parent: "#hero-two",
                 title: "P1",
@@ -26,76 +29,83 @@
                 }
             }
         },
-
-        /*
-
-        "Total Installs": {
-            source: demoURL,
-            GaugeLabel: {
-                parent: "#hero-three",
-                title: "Clients Installed"
-            }
-        },
-        "Clients 1": {
-            source: demoURL,
-            GaugeGadget: {
-                parent: "#hero-three",
-                title: "Cl1"
-            }
-        },
         */
         /*
-        "New Message": {
-            source: demoURL,
+
+         "Total Installs": {
+         source: demoURL,
+         GaugeLabel: {
+         parent: "#hero-three",
+         title: "Clients Installed"
+         }
+         },
+         "Clients 1": {
+         source: demoURL,
+         GaugeGadget: {
+         parent: "#hero-three",
+         title: "Cl1"
+         }
+         },
+         */
+        /*
+         "New Message": {
+         source: demoURL,
+         TimeSeries: {
+         parent: '#g1-1',
+         title: 'New Message',
+         label_offset: 200,
+         label_columns: 2,
+         time_span_mins: 12,
+         observer: function (data) {
+         console.log("Time series observing ", data);
+         }
+         }
+         },
+         */
+        "Ram Usage": {
+            source: demoURLRamUsage,
             TimeSeries: {
-                parent: '#g1-1',
-                title: 'New Message',
-                label_offset: 200,
-                label_columns: 2,
-                time_span_mins: 12,
-                observer: function (data) {
-                    console.log("Time series observing ", data);
-                }
-            }
-        },
-        */
-        "Fork Rate": {
-            source: demoURL,
-            TimeSeries: {
-                title: 'Fork Rate',
+                title: 'Ram Usage',
                 warn: 600,
                 error: 800,
                 parent: '#g2-1'
             }
-        }
-        /*
-        "Foo Work": {
-            source: demoURL,
-            BarChart: {
+        },
+
+        "Cpu Usage": {
+            source: demoURLCpuUsage,
+            TimeSeries: {
+                title: 'Cpu Usage',
+                parent: '#g2-2'
+            }
+        },
+        "Hdd Usage": {
+            source: demoURLHddUsage,
+            TimeSeries: {
+                title: 'Hdd Usage',
                 parent: '#g2-3'
             }
         }
-        */
+
     };
 
 
-     var g = new Graphene;
-     // g.demo();
-     g.build(jsonDesc);
-
+    var g = new Graphene;
+    // g.demo();
+    g.build(jsonDesc);
 
 
     /*
-    var g = new Graphene;
-    g.discover('https://10.30.103.95:8443', 'benchbox',
-        function (i, url) {
-            return  '#g2-3'
-        },
-        function (description) {
-            g.build(description);
-            console.log(description)
+     var g = new Graphene;
+     g.discover('https://10.30.103.95:8443', 'benchbox',
+     function (i, url) {
+     return  '#g2-3'
+     },
+     function (description) {
+     g.build(description);
+     console.log(description)
 
-        })
-    */
+     })
+     */
 
 }).call(this);
